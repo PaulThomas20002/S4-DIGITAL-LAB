d00efa0f6274c5f3e4d3c4ca9355e8614eec7e80
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2022-07-27 20:08:34 +0530

    Create testbench.sv

---
+++ XNOR_GATE/testbench.sv
@@ -0,0 +1,16 @@
+module xnor_tb;
+      reg x,y;
+      wire z;
+  xnor_gate inst(.x(x),.y(y),.z(z));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    #30 $finish;
+  end
+ initial begin
+ x=0;    y=0;    #5;
+ x=0;    y=1;    #5; //#timedelay
+ x=1;    y=0;    #5;
+ x=1;    y=1;    #5;
+ end
+endmodule
